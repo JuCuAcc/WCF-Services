10dfc13ca5b336fa9be0e6934a02dc4c2e2ba49e
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2022-02-03 21:25:31 +0600

    Encryption

---
+++ EncryptionTest/EncryptionTest/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptionTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Trim method that trims a string from both ends by removing white spaces or a specified character or characters.
+            txtCypher.Text = Encrypt(txtPlain.Text.Trim(), txtKey.Text.Trim());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           txtPlain.Text= Decrypt(txtCypher.Text.Trim(), txtKey.Text.Trim());
+        }
+        public static string Decrypt(string TextToBeDecrypted, string Password)
+        {
+            // Rijndael class uses a symmetric key algorithm (Rijndael/AES) to encrypt and decrypt data.
+            // As long as encryption and decryption routines use the same parameters to generate the keys.
+            RijndaelManaged RijndaelCipher = new RijndaelManaged();
+         
+            string DecryptedData;
+
+            try
+            {
+                byte[] EncryptedData = Convert.FromBase64String(TextToBeDecrypted);
+
+                byte[] Salt = Encoding.ASCII.GetBytes(Password.Length.ToString());
+                //Making of the key for decryption
+                // PasswordDeriveBytes Initializes a new instance of the PasswordDeriveBytes class specifying the password,
+                PasswordDeriveBytes SecretKey = new PasswordDeriveBytes(Password, Salt);
+                //Creates a symmetric Rijndael decryptor object.
+                ICryptoTransform Decryptor = RijndaelCipher.CreateDecryptor(SecretKey.GetBytes(32), SecretKey.GetBytes(16));
+
+                // The MemoryStream class creates streams that have memory as a backing store instead of a disk.
+                MemoryStream memoryStream = new MemoryStream(EncryptedData);
+                // CryptoStream is designed to perform transformation from a stream to another stream only and allows transformations chaining.
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, Decryptor, CryptoStreamMode.Read);
+
+                byte[] PlainText = new byte[EncryptedData.Length];
+                int DecryptedCount = cryptoStream.Read(PlainText, 0, PlainText.Length);
+                memoryStream.Close();
+                cryptoStream.Close();
+
+                //Converting to string
+                DecryptedData = Encoding.Unicode.GetString(PlainText, 0, DecryptedCount);
+            }
+            catch
+            {
+                DecryptedData = TextToBeDecrypted;
+            }
+            return DecryptedData;
+        }
+
+        public static string Encrypt(string TextToBeEncrypted, string Password)
+        {
+            RijndaelManaged RijndaelCipher = new RijndaelManaged();
+           
+            byte[] PlainText = System.Text.Encoding.Unicode.GetBytes(TextToBeEncrypted);
+            byte[] Salt = Encoding.ASCII.GetBytes(Password.Length.ToString());
+            PasswordDeriveBytes SecretKey = new PasswordDeriveBytes(Password, Salt);
+            //Creates a symmetric encryptor object. 
+            ICryptoTransform Encryptor = RijndaelCipher.CreateEncryptor(SecretKey.GetBytes(32), SecretKey.GetBytes(16));
+            MemoryStream memoryStream = new MemoryStream();
+            //Defines a stream that links data streams to cryptographic transformations
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, Encryptor, CryptoStreamMode.Write);
+            cryptoStream.Write(PlainText, 0, PlainText.Length);
+            //Writes the final state and clears the buffer
+            cryptoStream.FlushFinalBlock();
+            byte[] CipherBytes = memoryStream.ToArray();
+            memoryStream.Close();
+            cryptoStream.Close();
+            string EncryptedData = Convert.ToBase64String(CipherBytes);
+
+            return EncryptedData;
+        }
+    }
+}
